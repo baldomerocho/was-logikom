55be005db7bafac3832707495e7b5ab438916093
Baldomero Cho <don.baldo@hotmail.com>
2024-06-13 15:18:32 -0600

    upgrade 1.1.2

--- was-logikom.php
+++ was-logikom.php
@@ -3,7 +3,7 @@
 * Plugin Name: WAS Logikom
 * Plugin URI: https://logikom.com
 * Description: Send notifications to WhatsApp. Powered by Logikom
-* Version: 1.1.1
+* Version: 1.1.2
 * Author: Baldomero Cho
 * Author URI: https://datogedon.com
 **/
